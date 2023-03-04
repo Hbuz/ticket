5cd9c8c5f70ac753ce7d12e19197271d0cac59a4
Marco <marco.basilico@prowarehouse.nl>
2023-03-04 17:14:38 +0100

    Test commit

--- src/main/java/com/marco/ticket/TicketApplication.java
+++ src/main/java/com/marco/ticket/TicketApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketApplication {
 
+    // Test
     public static void main(String[] args) {
         SpringApplication.run(TicketApplication.class, args);
     }
